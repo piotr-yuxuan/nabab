9ad46bd7ad651d1b8e2358e8a8c4a00ae0929cef
Walter CI <piotr-yuxuan@users.noreply.github.com>
2022-07-01 06:45:15 +0000

    Update dependency versions

--- project.clj
+++ project.clj
@@ -15,7 +15,7 @@
   :aot :all
   :profiles {:github {:github/topics ["core-async" "clojure" "async" "channels"]
                       :github/private? false}
-             :provided {:dependencies [[org.clojure/clojure "1.11.1"]
+             :provided {:dependencies [[org.clojure/clojure "1.12.0-alpha1"]
                                        [io.confluent/kafka-avro-serializer "7.1.2"]
                                        [org.apache.avro/avro "1.11.0"]]}
              :dev {:global-vars {*warn-on-reflection* true}}}

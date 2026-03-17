f978ec3b8fd0d3e112c3b34bdd5e03aa72abb064
Walter CI <piotr-yuxuan@users.noreply.github.com>
2026-03-17 07:35:04 +0000

    Update dependency versions

--- project.clj
+++ project.clj
@@ -9,7 +9,7 @@
   :pom-addition [:developers [:developer
                               [:name "胡雨軒 Петр"]
                               [:url "https://github.com/piotr-yuxuan"]]]
-  :dependencies [[org.clojure/core.async "1.9.847-alpha3"]]
+  :dependencies [[org.clojure/core.async "1.9.859-alpha4"]]
   :plugins [[lein-cljsbuild "1.1.8"]
             [lein-shell "0.5.0"]]
   :aot :all
